716540f24718e9d155afadec44db7c08f5970660
juyingnan <bunny_gg@hotmail.com>
2014-01-26 21:06:34 +0800

    sh created

--- ln_config.sh
+++ ln_config.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # Get current path
-PWD = pwd 
+PWD=`pwd` 
 echo "Current path is "$PWD
 
 # zsh config
 sudo rm ~/.zshrc
 ln -s $PWD/zshrc ~/.zshrc
+echo "Link zsh file to current user path."
 sudo rm /etc/zsh/zshrc
 ln -s $PWD/zshrc /etc/zsh/zshrc
+echo "Link zsh file to etc path."
